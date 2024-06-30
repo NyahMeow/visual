3f0e411958c5c342b3a7099ebbd8f3e405b891d6
NyahMeow <154108203+NyahMeow@users.noreply.github.com>
2024-07-01 02:25:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -112,50 +112,52 @@ function createChart(dataArray) {
     console.log(chart); // チャートオブジェクトの確認
 }
 
- // 3D散布図のマウスイベントを追加
-    (function (H) {
-        function dragStart(eStart) {
-            eStart = chart.pointer.normalize(eStart);
+// Some magic here for the interactive feattures
+//Add mouse and touch events for rotation
+(function (H) {
+    function dragStart(eStart) {
+        eStart = chart.pointer.normalize(eStart);
 
-            var posX = eStart.chartX,
-                posY = eStart.chartY,
-                alpha = chart.options.chart.options3d.alpha,
-                beta = chart.options.chart.options3d.beta,
-                sensitivity = 5,  // lower is more sensitive
-                handlers = [];
+        var posX = eStart.chartX,
+            posY = eStart.chartY,
+            alpha = chart.options.chart.options3d.alpha,
+            beta = chart.options.chart.options3d.beta,
+            sensitivity = 5,  // lower is more sensitive
+            handlers = [];
 
-            function drag(e) {
-                // Get e.chartX and e.chartY
-                e = chart.pointer.normalize(e);
+        function drag(e) {
+            // Get e.chartX and e.chartY
+            e = chart.pointer.normalize(e);
 
-                chart.update({
-                    chart: {
-                        options3d: {
-                            alpha: alpha + (e.chartY - posY) / sensitivity,
-                            beta: beta + (posX - e.chartX) / sensitivity
-                        }
+            chart.update({
+                chart: {
+                    options3d: {
+                        alpha: alpha + (e.chartY - posY) / sensitivity,
+                        beta: beta + (posX - e.chartX) / sensitivity
                     }
-                }, undefined, undefined, false);
-            }
-
-            function unbindAll() {
-                handlers.forEach(function (unbind) {
-                    if (unbind) {
-                        unbind();
-                    }
-                });
-                handlers.length = 0;
-            }
-
-            handlers.push(H.addEvent(document, 'mousemove', drag));
-            handlers.push(H.addEvent(document, 'touchmove', drag));
-            handlers.push(H.addEvent(document, 'mouseup', unbindAll));
-            handlers.push(H.addEvent(document, 'touchend', unbindAll));
+                }
+            }, undefined, undefined, false);
         }
-        H.addEvent(chart.container, 'mousedown', dragStart);
-        H.addEvent(chart.container, 'touchstart', dragStart);
-    }(Highcharts));
-}
+
+        function unbindAll() {
+            handlers.forEach(function (unbind) {
+                if (unbind) {
+                    unbind();
+                }
+            });
+            handlers.length = 0;
+        }
+
+        handlers.push(H.addEvent(document, 'mousemove', drag));
+        handlers.push(H.addEvent(document, 'touchmove', drag));
+        handlers.push(H.addEvent(document, 'mouseup', unbindAll));
+        handlers.push(H.addEvent(document, 'touchend', unbindAll));
+    }
+    H.addEvent(chart.container, 'mousedown', dragStart);
+    H.addEvent(chart.container, 'touchstart', dragStart);
+    
+  
+}(Highcharts));
 
 
 // 初期チャートの設定とデフォルトデータ
