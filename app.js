a0dbe594b441f57cef862b00056acd467eaa355f
NyahMeow <154108203+NyahMeow@users.noreply.github.com>
2024-07-01 18:02:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM fully loaded and parsed");
     document.getElementById('analyzeButton').addEventListener('click', processFile);
 });
